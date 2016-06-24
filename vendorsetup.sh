27f0c5681eec51d3190adba9a9ff6aba7bc01e20
JohnHoder <hoder.john@gmail.com>
2016-06-25 00:01:07 +0200

    Cleanup
    Change-Id: I641f3df45eca9ffc01231c378c08a76726fd860e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
-for combo in $(curl -s https://raw.githubusercontent.com/androidarmv6/hudson/master/androidarmv6-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
-do
-    add_lunch_combo $combo
-done
+add_lunch_combo perferom_cooper-userdebug
+
